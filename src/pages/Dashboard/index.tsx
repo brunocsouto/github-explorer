c0f47f12666321f0e6146dad1910dd2916df0a72
Bruno Souto <bruno.csouto@gmail.com>
2020-07-05 22:14:08 -0300

    feat: dashboard and repository explorer

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,99 @@
-import React from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório.');
+      return;
+    }
+
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca por esse repositório');
+    }
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite aqui o nome do repositório" />
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite aqui o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
+      {inputError && <Error>{inputError}</Error>}
+
       <Repositories>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/6605242?s=460&u=b1ebc5006a4743c933c1bfdae90c10f4d555c701&v=4"
-            alt="Github Avatar"
-          />
-          <div>
-            <strong>brunocsouto/rick-morty-multiverse</strong>
-            <p>React app with information about Rick and Morty&apos;s series</p>
-          </div>
+        {repositories.map(repository => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/6605242?s=460&u=b1ebc5006a4743c933c1bfdae90c10f4d555c701&v=4"
-            alt="Github Avatar"
-          />
-          <div>
-            <strong>brunocsouto/rick-morty-multiverse</strong>
-            <p>React app with information about Rick and Morty&apos;s series</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="test">
-          <img
-            src="https://avatars2.githubusercontent.com/u/6605242?s=460&u=b1ebc5006a4743c933c1bfdae90c10f4d555c701&v=4"
-            alt="Github Avatar"
-          />
-          <div>
-            <strong>brunocsouto/rick-morty-multiverse</strong>
-            <p>React app with information about Rick and Morty&apos;s series</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
       </Repositories>
     </>
   );
